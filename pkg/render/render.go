ab7f08e003763c9b4c5f579b79afead30985d7a7
colinpeacock1 <37456754+colinpeacock1@users.noreply.github.com>
2021-03-18 07:49:29 +0000

    initial commit

---
+++ pkg/render/render.go
@@ -0,0 +1,90 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/colinpeacock1/bookings/pkg/config"
+	"github.com/colinpeacock1/bookings/pkg/models"
+)
+
+var functions = template.FuncMap{}
+var app *config.AppConfig
+
+// NewTemplate sets the config for the template package
+func NewTemplate(a *config.AppConfig) {
+	app = a
+}
+
+// AddDefaultData generates default data
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// Template renders templates using html/template
+func Template(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	// get the template cache from the app config
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+	_ = t.Execute(buf, td)
+
+	_, err := buf.WriteTo(w)
+
+	if err != nil {
+		fmt.Println("error writing template to browser", err)
+		return
+	}
+}
+
+// CreateTemplateCache creates template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page is currently", page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+		}
+		if err != nil {
+			return myCache, err
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
